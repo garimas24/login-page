d9579877736d0601cc305fb5b49b39c5a08c75f5
unknown <dixitgarima24@gmail.com>
2024-09-28 21:24:35 +0530

    errors

--- src/XLogin.jsx
+++ src/XLogin.jsx
@@ -16,9 +16,10 @@ function XLogin() {
 
   return (
     <div>
+      <h1>Login Page</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
@@ -28,7 +29,7 @@ function XLogin() {
           />
         </div>
         <div>
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
